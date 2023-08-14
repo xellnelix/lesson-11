5e807ab3bf82cbe8b01a24cbc02e740982553965
xellnelix <xellnelix@gmail.com>
2023-08-14 20:22:23 +0300

    Rewrited swim method

--- src/main/java/animals/Horse.java
+++ src/main/java/animals/Horse.java
@@ -1,25 +1,7 @@
 package animals;
 
 public class Horse extends Animal {
-    public Horse(String name, int speedOfRun, int speedOfSwim, int stamina) {
-        super(name, speedOfRun, speedOfSwim, stamina);
-    }
-
-    @Override
-    public double swim(int distance) {
-        if (stamina < distance * 4) {
-            System.out.println("Нельзя выполнить действие: проплыть на " + distance + " м. с запасом выносливости " + stamina + "\n");
-            return -1;
-        }
-        if (distance == 0) {
-            System.out.println(name + " стоит на месте\n");
-            return 0;
-        }
-        stamina -= distance * 4;
-        if (stamina <= 0) {
-            isTired = true;
-        }
-        System.out.println(name + " проплыл(а) " + distance + " м. затратив " + distance * 4 + " у.е. выносливости\n");
-        return (double) distance / speedOfSwim;
-    }
+	public Horse(String name, int speedOfRun, int speedOfSwim, int stamina) {
+		super(name, speedOfRun, speedOfSwim, stamina, 4);
+	}
 }
